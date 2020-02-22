72b4c7c42e4f6b67d979233ac9b95e5974cfb4d4
itaknfp <60814410+itaknfp@users.noreply.github.com>
2020-02-22 16:21:35 +0700

    Add files via upload

--- w3.py
+++ w3.py
@@ -4,16 +4,18 @@ total = 0
 print ('MENGHITUNG NILAI RATA-RATA')
 n = int(input("Masukkan banyaknya siswa: "))
 
-for x in range(n):
+for x in range(n-1):
     nilai = float(input("Masukkan nilai siswa ke-{} : ".format(x+1)))
     array.append(nilai)
-rata = (sum(array)/n )   
 kamu = int(input("Masukkan nilai kamu: "))
+rata = (sum(array) + kamu)/ n
+
 print("Hasil rata-rata adalah : ",rata)
 
 if kamu < rata:
     print ('nilaimu dibawah rata-rata')
 elif kamu == rata :
-    print ('nilaimu sama dengan rata-rata',x)
+    print ('nilaimu sama dengan rata-rata')
 else:
     print ('nilaimu diatas rata-rata')
+
